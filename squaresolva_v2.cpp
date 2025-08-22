1a154aa17fd98cb9a34a1ad7fa88642241258e41
ishut-nub <denis.mironov@autogramma.ru>
2025-08-22 16:52:32 +0300

    fixed bugs and change the overall look

--- squaresolva_v2.cpp
+++ squaresolva_v2.cpp
@@ -1,26 +1,31 @@
 #include <stdio.h>
 #include <math.h>
+
 int main(void)
 {
-	double a=0,b=0,c=0;
-	double x1=0,x2=0,d=0;
-	scanf("%lg %lg %lg",&a,&b,&c);
-	d=b*b-4*a*c;
-	x1=(-b+sqrt(d))/(2*a);
-	x2=(-b-sqrt(d))/(2*a);
-	if (d<0)
-		printf("Equation has no real roots\n");
-	else if (d==0)
-		printf("Equation has one solution, %lg\n", -b/(2*a));
-	else
-	{
-	if (a==0 && b==0 && c==0)
-                printf("Equation has infinite solutions\n");
-        else if (a==0 && b!=0 && c!=0)
-                printf("Equation is not quadratic, one solution - %lg\n", -c/b);
-	else
-		printf("Equation has 2 solutions, %lg and %lg\n", x1,x2);
-	}
-	return 0;
+    double coef_a = 0, coef_b = 0, coef_c = 0;
+    double root1 = 0, root2 = 0, dis = 0;
+
+    printf("Enter equation coefficents: ");
+    scanf("%lg %lg %lg", &coef_a, &coef_b, &coef_c);
+
+    if (coef_a == 0 && coef_b == 0 && coef_c == 0)
+        printf("Equation has infinite solutions\n");
+    else if (coef_a == 0 && coef_b != 0 && coef_c != 0)
+        printf("Equation has one solution, %lg\n", -coef_c / coef_b);
+    else
+    {
+        dis = coef_b * coef_b - 4 * coef_a * coef_c;
+        if (dis < 0)
+            printf("Equation has no solutions\n");
+        else if (dis==0)
+            printf("Equation has 1 solution, %lg\n", (-coef_b/(2*coef_a)));
+        else
+        {
+            root1 = (-coef_b + sqrt(dis)) / (2 * coef_a);
+            root2 = (-coef_b - sqrt(dis)) / (2 * coef_a);
+            printf("Equation has 2 solutions, %lg and %lg\n", root1, root2);
+        }
+    }
 }
 
