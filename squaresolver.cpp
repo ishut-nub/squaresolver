c1fb196a0cfd7d450b68ed7226e665a9fc5a270d
ishut-nub <denis.mironov@autogramma.ru>
2025-08-23 19:40:34 +0300

    added more functions

--- squaresolver.cpp
+++ squaresolver.cpp
@@ -1,77 +1,96 @@
 #include <stdio.h>
 #include <math.h>
+#define INF_ROOTS -1
+#define NO_ROOTS 0
+#define ONE_ROOT 1
+#define TWO_ROOTS 2
 
-void input(void);
-int count_solves(double a, double b, double c, double * root_1, double * root_2);
-void output(int root_count, double * root_1, double * root_2);
+void input(double *coeff_a, double *coeff_b, double *coeff_c);
+int linear_equation(double coeff_b, double coeff_c, double *root_1);
+int quadratic_equation(double coeff_a, double coeff_b, double coeff_c, double *root_1, double *root_2);
+int solver(double coeff_a, double coeff_b, double coeff_c, double *root_1, double *root_2);
+void output(int root_count, double *root_1, double *root_2);
 
 int main(void)
 {
     double coeff_a = 0, coeff_b = 0, coeff_c = 0;
     double root_1 = 0, root_2 = 0;
     int root_count = 0;
-    input();
-    scanf("%lg %lg %lg", &coeff_a, &coeff_b, &coeff_c); //warning: ignoring return value of ‘int scanf(const char*, ...)’ declared with attribute ‘warn_unused_result’ [-Wunused-result]
-    // printf("%lg %lg %lg\n", coeff_a, coeff_b, coeff_c); //checking
-    root_count = count_solves(coeff_a, coeff_b, coeff_c, &root_1, &root_2);
+    input(&coeff_a, &coeff_b, &coeff_c);
+    root_count = solver(coeff_a, coeff_b, coeff_c, &root_1, &root_2);
     output(root_count, &root_1, &root_2);
 }
 
-void input(void)
+void input(double *coeff_a, double *coeff_b, double *coeff_c)
 {
     printf("Your equation will look like this: ax^2+bx+c\n");
     printf("Please enter a, b and c coefficents: ");
+    while (scanf("%lg %lg %lg", coeff_a, coeff_b, coeff_c)!=3)
+    {
+        printf("Incorrect input. Try again\n");
+        scanf("%lg %lg %lg", coeff_a, coeff_b, coeff_c);
+    }
 }
 
-int count_solves(double a, double b, double c, double * root_1, double * root_2)
+void output(int root_count, double *root_1, double *root_2)
 {
-    if (a == 0)
+    switch (root_count)
     {
-        if (b == 0)
-        {
-            if (c == 0)
-                return -1;
-            else
-                return 0;
+        case INF_ROOTS: printf("Infinite amount of solutions\n");
+                 break;
+        case NO_ROOTS:  printf("No solutions\n");
+                 break;
+        case ONE_ROOT:  printf("Equation has 1 solution, %lg\n", *root_1);
+                 break;
+        case TWO_ROOTS:  printf("Equation has 2 solutions, %lg and %lg\n", *root_1, *root_2);
+                 break;
+        default: printf("How did you even get there?\n");
+    }
+}
+int linear_equation(double coeff_b, double coeff_c, double *root_1)
+{
+    if (coeff_b == 0)
+    {
+        if (coeff_c == 0) {
+            return INF_ROOTS;
         }
-        else
-        {
-            *root_1 = (-c/b);
-            return 1;
+        else {
+            return NO_ROOTS;
         }
     }
     else
     {
-        int discriminant = 0;
-        discriminant = b * b - 4 * a * c;
-        if (discriminant > 0)
-        {
-            *root_1 = (-b + sqrt(discriminant))/(2*a);
-            *root_2 = (-b - sqrt(discriminant))/(2*a);
-            return 2;
-        }
-        else if (discriminant == 0)
-        {
-            *root_1 = *root_2 = (-b/(2*a));
-            return 1;
-        }
-        else
-            return 0;
+        *root_1 = (-coeff_c/coeff_b);
+        return ONE_ROOT;
     }
 }
 
-void output(int root_count, double * root_1, double * root_2)
+int quadratic_equation(double coeff_a, double coeff_b, double coeff_c, double *root_1, double *root_2)
 {
-    switch (root_count)
+    double discriminant = coeff_b * coeff_b - 4 * coeff_a * coeff_c;
+
+    if (discriminant > 0)
     {
-        case -1: printf("Infinite amount of solutions\n");
-                 break;
-        case 0:  printf("No solutions\n");
-                 break;
-        case 1:  printf("Equation has 1 solution - %lg\n", *root_1);
-                 break;
-        case 2:  printf("Equation has 2 solutions - %lg and %lg\n", *root_1, *root_2);
-                 break;
-        default: printf("How did you even get there?");
+        *root_1 = (-coeff_b + sqrt(discriminant))/(2*coeff_a);
+        *root_2 = (-coeff_b - sqrt(discriminant))/(2*coeff_a);
+        return TWO_ROOTS;
+    }
+    else if (discriminant == 0)
+    {
+        *root_1 = *root_2 = (-coeff_b/(2*coeff_a));
+        return ONE_ROOT;
+    }
+    else {
+        return NO_ROOTS;
+    }
+}
+
+int solver(double coeff_a, double coeff_b, double coeff_c, double *root_1, double *root_2)
+{
+    if (coeff_a == 0) {
+        return linear_equation(coeff_b, coeff_c, root_1);
+        }
+    else {
+        return quadratic_equation(coeff_a, coeff_b, coeff_c, root_1, root_2);
     }
 }
