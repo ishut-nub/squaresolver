da11418523f5fdb3175dd9490b04e6de41049465
ishut-nub <denis.mironov@autogramma.ru>
2025-08-23 14:53:59 +0300

    added functions(input, solver, output)

--- squaresolva.cpp
+++ squaresolva.cpp
@@ -1,16 +1,77 @@
 #include <stdio.h>
-#include <TXLib.h>
 #include <math.h>
-int main()
+
+void input(void);
+int count_solves(double a, double b, double c, double * root_1, double * root_2);
+void output(int root_count, double * root_1, double * root_2);
+
+int main(void)
 {
-    int a,b,c;      //объявление переменных a b c
-    double x1,x2,d; //объявление переменных x1 x2 d с плавающей точкой
-    printf("Ваше уравнение будет выглядеть так: ax^2+bx+c=0\n");
-    printf("Введите коэффиценты a, b и c через пробел\n");
-    scanf("%d %d %d", &a,&b,&c);   //запрос на a b c
-    d=pow(pow(b,2)-4*a*c,0.5);     //рассчет дискриминанта
-    x1=(-b+d)/(2*a);               //первый корень
-    x2=(-b-d)/(2*a);               //второй корень
-    printf("Корни уравнения равны %f и %f\n",x1,x2);
-    return 0;
+    double coeff_a = 0, coeff_b = 0, coeff_c = 0;
+    double root_1 = 0, root_2 = 0;
+    int root_count = 0;
+    input();
+    scanf("%lg %lg %lg", &coeff_a, &coeff_b, &coeff_c); //warning: ignoring return value of вЂint scanf(const char*, ...)вЂ™ declared with attribute вЂwarn_unused_resultвЂ™ [-Wunused-result]
+    // printf("%lg %lg %lg\n", coeff_a, coeff_b, coeff_c); //checking
+    root_count = count_solves(coeff_a, coeff_b, coeff_c, &root_1, &root_2);
+    output(root_count, &root_1, &root_2);
+}
+
+void input(void)
+{
+    printf("Your equation will look like this: ax^2+bx+c\n");
+    printf("Please enter a, b and c coefficents: ");
+}
+
+int count_solves(double a, double b, double c, double * root_1, double * root_2)
+{
+    if (a == 0)
+    {
+        if (b == 0)
+        {
+            if (c == 0)
+                return -1;
+            else
+                return 0;
+        }
+        else
+        {
+            *root_1 = (-c/b);
+            return 1;
+        }
+    }
+    else
+    {
+        int discriminant = 0;
+        discriminant = b * b - 4 * a * c;
+        if (discriminant > 0)
+        {
+            *root_1 = (-b + sqrt(discriminant))/(2*a);
+            *root_2 = (-b - sqrt(discriminant))/(2*a);
+            return 2;
+        }
+        else if (discriminant == 0)
+        {
+            *root_1 = *root_2 = (-b/(2*a));
+            return 1;
+        }
+        else
+            return 0;
+    }
+}
+
+void output(int root_count, double * root_1, double * root_2)
+{
+    switch (root_count)
+    {
+        case -1: printf("Infinite amount of solutions\n");
+                 break;
+        case 0:  printf("No solutions\n");
+                 break;
+        case 1:  printf("Equation has 1 solution - %lg\n", *root_1);
+                 break;
+        case 2:  printf("Equation has 2 solutions - %lg and %lg\n", *root_1, *root_2);
+                 break;
+        default: printf("How did you even get there?");
+    }
 }
