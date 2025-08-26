bf2413e3decaee87588dfc473558834dfaa57dbe
ishut-nub <denis.mironov@autogramma.ru>
2025-08-26 15:54:10 +0300

    final (hopefully) version (fixed tests, can solve multiple equations)

--- squaresolver.cpp
+++ squaresolver.cpp
@@ -11,7 +11,7 @@ enum root_counter
     TWO_ROOTS = 2
 };
 
-const double ACCURACY_LIMIT = 0.00001;
+const double ACCURACY_LIMIT = 1e-15;
 
 struct test_data
 {
@@ -28,6 +28,8 @@ struct equation_info
 };
 
 void input_coeffs(equation_info *info);
+void output_roots(equation_info *info);
+int another_equation(void);
 
 int linear_equation(equation_info *info);
 int quadratic_equation(equation_info *info);
@@ -43,24 +45,28 @@ int clear_buffer(void);
 
 int solver_tester(test_data *test, equation_info *info);
 int run_test(equation_info *info);
-
-void output_roots(equation_info *info);
+int compare_test_roots(test_data *test, equation_info *info);
 
 int main(void)
 {
-    struct equation_info info = {.coeff_a = NAN, .coeff_b = NAN, .coeff_c = NAN, .root_1 = NAN, .root_2 = NAN, .root_count = NO_ROOTS};
-    input_coeffs(&info);
+    printf("miau\n\n");
+    struct equation_info info = {.coeff_a = NAN, .coeff_b = NAN, .coeff_c = NAN,
+                                 .root_1 = NAN, .root_2 = NAN, .root_count = NO_ROOTS};
 
-//    assert(!(isnan(info.coeff_a)));
-//    assert(!(isnan(info.coeff_b)));
-//    assert(!(isnan(info.coeff_c)));
+    int result = run_test(&info);
+    printf("Failed %d tests\n\n", result);
+
+    restart:
+
+    input_coeffs(&info);
 
     info.root_count = solver(&info);
 
     output_roots(&info);
 
-    int result = run_test(&info);
-    printf("Failed %d tests\n", result);
+    if (another_equation())
+        goto restart;
+    printf("COMMIT GITHUB, LEAVE BUILDING\n");
     return 0;
 }
 
@@ -149,8 +155,10 @@ int quadratic_equation(equation_info *info)
 
     if (discriminant > 0)
     {
-        info->root_1 = find_min((-info->coeff_b + sqrt_discriminant) / (2 * info->coeff_a), (-info->coeff_b - sqrt_discriminant) / (2 * info->coeff_a));
-        info->root_2 = find_max((-info->coeff_b + sqrt_discriminant) / (2 * info->coeff_a), (-info->coeff_b - sqrt_discriminant) / (2 * info->coeff_a));
+        info->root_1 = find_min((-info->coeff_b + sqrt_discriminant) / (2 * info->coeff_a),
+                                (-info->coeff_b - sqrt_discriminant) / (2 * info->coeff_a));
+        info->root_2 = find_max((-info->coeff_b + sqrt_discriminant) / (2 * info->coeff_a),
+                                (-info->coeff_b - sqrt_discriminant) / (2 * info->coeff_a));
         return TWO_ROOTS;
     }
     else if (compare_double(discriminant, 0))
@@ -166,6 +174,10 @@ int quadratic_equation(equation_info *info)
 
 int solver(equation_info *info)
 {
+    assert(!(isnan(info->coeff_a)));
+    assert(!(isnan(info->coeff_b)));
+    assert(!(isnan(info->coeff_c)));
+
     if (compare_double(info->coeff_a, 0)) {
         return linear_equation(info);
     }
@@ -183,14 +195,46 @@ int compare_double(double double_a, double double_b)
     return (fabs(double_a - double_b) < ACCURACY_LIMIT);
 }
 
+double find_min(double min_1, double min_2)
+{
+    if (!(isnan(min_1)) && (isnan(min_2)))
+        return min_1;
+    if (isnan(min_1) && (!(isnan(min_2))))
+        return min_2;
+    if (isnan(min_1) && isnan(min_2))
+        return min_1;
+    return fmin(min_1, min_2);
+}
+
+double find_max(double max_1, double max_2)
+{
+    if (!(isnan(max_1)) && (isnan(max_2)))
+        return max_1;
+    if (isnan(max_1) && (!(isnan(max_2))))
+        return max_2;
+    if (isnan(max_1) && isnan(max_2))
+        return max_1;
+    return fmax(max_1, max_2);
+}
+
 int solver_tester(test_data *test, equation_info *info)
 {
+    info->coeff_a = test->compare_coeff_a;
+    info->coeff_b = test->compare_coeff_b;
+    info->coeff_c = test->compare_coeff_c;
+
+    assert(!(isnan(info->coeff_a)));
+    assert(!(isnan(info->coeff_b)));
+    assert(!(isnan(info->coeff_c)));
+
     info->root_count = solver(info);
 
-    if (!(compare_double(find_min(info->root_1, info->root_2), find_min(test->compare_root_1, test->compare_root_2)) && compare_double(find_max(info->root_1, info->root_2),find_max(test->compare_root_1, test->compare_root_2)) && info->root_count == test->compare_root_count))
+    if (!(compare_test_roots(test, info) && (info->root_count == test->compare_root_count)))
     {
         printf("Test failed \nRoots %lg %lg instead of %lg %lg \n"
-               "Number of roots %d instead of %d\n", info->root_1, info->root_2, test->compare_root_1, test->compare_root_2, info->root_count, test->compare_root_count);
+               "Number of roots %d instead of %d\n\n",
+               info->root_1, info->root_2, test->compare_root_1, test->compare_root_2,
+               info->root_count, test->compare_root_count);
         return 1;
     }
     else
@@ -219,24 +263,31 @@ int run_test(equation_info *info)
     return test_failed;
 }
 
-double find_min(double min_1, double min_2)
+int compare_test_roots(test_data *test, equation_info *info)
 {
-    if (!(isnan(min_1)) && (isnan(min_2)))
-        return min_1;
-    if (isnan(min_1) && (!(isnan(min_2))))
-        return min_2;
-    if (isnan(min_1) && isnan(min_2))
-        return min_1;
-    return fmin(min_1, min_2);
+    double min_root = NAN;
+    double max_root = NAN;
+    double min_compare_root = NAN;
+    double max_compare_root = NAN;
+
+    min_root = find_min(info->root_1, info->root_2);
+    max_root = find_max(info->root_1, info->root_2);
+    min_compare_root = find_min(test->compare_root_1, test->compare_root_2);
+    max_compare_root = find_max(test->compare_root_1, test->compare_root_2);
+
+    if (compare_double(min_root, min_compare_root) && compare_double(max_root, max_compare_root))
+        return 1;
+    return 0;
 }
 
-double find_max(double max_1, double max_2)
+int another_equation(void)
 {
-    if (!(isnan(max_1)) && (isnan(max_2)))
-        return max_1;
-    if (isnan(max_1) && (!(isnan(max_2))))
-        return max_2;
-    if (isnan(max_1) && isnan(max_2))
-        return max_1;
-    return fmax(max_1, max_2);
+    char ch = 0;
+
+    printf("Solve another equation? [y/n] ");
+    scanf("%c", &ch);
+
+    if (ch == 'y')
+        return 1;
+    return 0;
 }
